b935a5d074f390f7de9dd7d5c40cf2933bbc39f9
Rakonczai Sándor <rakonczai.sandor@outlook.hu>
2014-08-18 18:49:49 +0200

    Removed files.

--- winjs.todo/Application.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace winjs.todo
-
-open IntelliFactory.WebSharper
-
-[<JavaScript>]
-type Application =
-    
-    [<Inline "WinJS.Application.onready = $0">]
-    static member OnReady (a : unit -> 'a) = X<unit>
